2fe29aaa5815d65f700e19cb5690ebbc1691d990
rtahir1995 <mail.to@tahir-rahimov.com>
2021-07-21 22:06:26 +0400

    Demo Version

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,14 +16,50 @@ $('.signInSVG').find('a').on('click', function(e) {
 $('.mobileS3').on('click', function () {
     
     $('.navigationMob').animate({
-        width: 0,
+        backgroundPositionX: '0',
     }, 500, function() {
         $(".navigationMob").css({
             display: 'none'
         }).animate({
             opacity: 1
-        }, 1000);
+        }, 500);
+    });
 
+    $('.mobileNav').find('svg').animate({
+        // height: '30px',
+    }, 1000, function() {
+        $(".mobileNav").css({
+            display: 'flex'
+        }).animate({
+            opacity: 1
+        }, 1000);
     });
     
 });
+
+
+$('.mobileNav').on('click', function () {
+    
+    $('.navigationMob').animate({
+        // width: 0,
+    }, 500, function() {
+        $(".navigationMob").css({
+            display: 'block'
+        }).animate({
+            opacity: 1
+        }, 500);
+    });
+
+    $('.mobileNav').find('svg').animate({
+        // height: '0px',
+    }, 500, function() {
+        $(".mobileNav").css({
+            display: 'none'
+        }).animate({
+            opacity: 1
+        }, 500);
+    });
+    
+});
+
+
