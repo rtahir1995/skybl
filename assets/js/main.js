aec3d9e7a19909b643720562391b6a4c0c24c62b
rtahir1995 <mail.to@tahir-rahimov.com>
2021-07-21 20:54:47 +0400

    Demo Version

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,23 @@ $('.activeNav').on('click', function(e) {
 $('.activeButton').on('click', function(e) {
     e.preventDefault();
 });
+
+$('.signInSVG').find('a').on('click', function(e) {
+    e.preventDefault();
+});
+
+
+$('.mobileS3').on('click', function () {
+    
+    $('.navigationMob').animate({
+        width: 0,
+    }, 500, function() {
+        $(".navigationMob").css({
+            display: 'none'
+        }).animate({
+            opacity: 1
+        }, 1000);
+
+    });
+    
+});
