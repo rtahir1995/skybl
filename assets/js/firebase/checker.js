db6963c1523ba9e0ee0f198fc13884e68b835c87
Aykhan85 <ayxannematov96@gmail.com>
2021-07-30 23:31:24 +0400

    ayxan version

--- assets/js/firebase/checker.js
+++ assets/js/firebase/checker.js
@@ -4,9 +4,16 @@ $(document).ready(function () {
     if(!keyword){
         $('.pupupSection').toggle('slow'); 
        // window.location.href = "login.html";
+       $(".post").css({
+           display: "none"
+       });
+       $(".logout").css({
+        display: "none"
+    })
         return
     };
- 
+
+   
     var db = firebase.database();
  
     db.ref("users/" + keyword).once("value", function (snapshot) {
@@ -21,6 +28,16 @@ $(document).ready(function () {
          window.userInfo = info
     })
 
+    
+    $(".post").css({
+        display: "block"
+    });
+
+    $(".logout").css({
+        display: "block"
+    })
+ 
+
     $('.signUpForm').css({
         display: 'none'
     });
@@ -28,4 +45,18 @@ $(document).ready(function () {
     $('.logInForm').css({
        display: 'none'
    });
+
+   $(".logout").on("click", function () {
+    window.localStorage.removeItem("keyword");
+    $(".logout").css({
+        display: "none",
+    })
+    $('.logInForm').css({
+        display: 'block'
+    });
+    $(".post").css({
+        display: "none"
+    });
+})
+
  })
